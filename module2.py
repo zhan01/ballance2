612cbeac2fe2a36633b656ab0cfd1ca9b9039b7d
zhan <zhunussov1994@gmail.com>
2019-07-30 15:07:29 +0600

    Second commit

--- module2.py
+++ module2.py
@@ -2,12 +2,9 @@ from module1 import balance, transactions
 
 def apply_transactions(balance, transactions):
     for transaction in transactions:
-        for key,val in transaction.items():
-            user = key
-            value = val
+        for user,value in transaction.items():
             balance[user] += value
     return balance
 
  
-newBalance = apply_transactions(balance, transactions)
-print(newBalance)
+print(apply_transactions(balance, transactions))
